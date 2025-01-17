e360f220d596bf3af06b752b2550e04af97a193a
Nasfurion <diego.amadio93@gmail.com>
2025-01-17 14:19:16 -0400

    Added ID

--- Lab1/Models/Person.cs
+++ Lab1/Models/Person.cs
@@ -4,6 +4,7 @@ namespace Lab1.Models
 {
     public class Person
     {
+        public int Id { get; set; }
         [Display (Name="First Name")]
         public string FirstName { get; set; } = string.Empty;
         [Display (Name = "Last Name")]
